c6add851644868b60c7ee1e96b12ccb8faf9bc3d
모두를 위한 인공지능 AI <85654856+dscoool@users.noreply.github.com>
2023-09-22 11:44:21 +0900

    Update sql_02.sql

--- 4_SELECT_2/sql_02.sql
+++ 4_SELECT_2/sql_02.sql
@@ -1,8 +1,8 @@
-SELECT * FROM cctv.freewifi;
+SELECT * FROM cctv.wifi;
 
 USE CCTV;
 
-INSERT INTO cctv.freewifi ( `num`, `add`, `add2`,
+INSERT INTO cctv.wifi ( `num`, `add`, `add2`,
   `city`, `district`, `object`, `provider`, `SSID`,
   `dd`, `add3`, `add4`, `org`, `phone`,
   `latitude`, `longitude`, `logdd`) VALUES ('256','번2동주민센터',
@@ -11,25 +11,25 @@ INSERT INTO cctv.freewifi ( `num`, `add`, `add2`,
   '37.6322163', '127.0387405', '2020-06-15');
   
   
-SELECT * FROM cctv.freewifi;
+SELECT * FROM cctv.wifi;
 
-DELETE FROM `cctv`.`freewifi` WHERE (`num`='256');
+DELETE FROM `cctv`.`wifi` WHERE (`num`='256');
 
-UPDATE `cctv`.`freewifi` SET `add2` = '1F 민원실' WHERE (`num` = '255');
+UPDATE `cctv`.`wifi` SET `add2` = '1F 민원실' WHERE (`num` = '255');
 
-SELECT * FROM cctv.freewifi WHERE (`add` = '보건소');
+SELECT * FROM cctv.wifi WHERE (`add` = '보건소');
 
-SELECT * FROM cctv.freewifi WHERE (`district` = '강북구');
+SELECT * FROM cctv.wifi WHERE (`district` = '강북구');
 
 -- SQL은 대소문자 구분 안함!! (보통 대문자를 씁니다)
-select `add`, `add2`, `SSID` from cctv.FREEWIFI where (`add` = '보건소');
+select `add`, `add2`, `SSID` from cctv.wifi where (`add` = '보건소');
 
 -- SELECT * 은 한 줄을 출력할 때 모든 열(COLUMN)을 보여주라는 의미입니다.
 -- SELECT `add`, `add2`, `SSID` 와 같이 3개 컬럼만 가져올 수도 있어요!! :)
   
-select `add`, `add2`, `SSID` from cctv.freewifi where (`add` = '보건소');
+select `add`, `add2`, `SSID` from cctv.wifi where (`add` = '보건소');
 
 -- 명령문 안에 들어가는 단어 ex: add 가 명령문과 겹치지 않는다면, 좌따옴표 ``를 생략해도 무방합니다!!
  
-select `add`, add2, SSID from cctv.freewifi where (`add` = '보건소');
+select `add`, add2, SSID from cctv.wifi where (`add` = '보건소');
 
